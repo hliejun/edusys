f08c69502aff0c5bb838e705e341adaee89c029b
Huang Lie Jun <liejun.hlj@gmail.com>
2019-05-08 10:40:05 +0800

    updated db config, fixed production start script

--- knexfile.js
+++ knexfile.js
@@ -5,20 +5,32 @@ dotenv.config({
 	path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`)
 });
 
+const connectionConfig = {
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	port: process.env.DB_PORT
+};
+
+const dataConfig = {
+	typeCast: function(field, next) {
+		if (field.type === 'TINY' && field.length === 1) {
+			return field.string() === '1';
+		}
+		return next();
+	}
+};
+
+if (process.env.DB_CONNECTION && process.env.NODE_ENV !== 'development') {
+	connectionConfig.socketPath = `/cloudsql/${process.env.DB_CONNECTION}`;
+}
+
 const config = {
 	client: process.env.DB_CLIENT,
 	connection: {
-		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASS,
-		database: process.env.DB_NAME,
-		port: process.env.DB_PORT,
-		typeCast: function(field, next) {
-			if (field.type === 'TINY' && field.length === 1) {
-				return field.string() === '1';
-			}
-			return next();
-		}
+		...connectionConfig,
+		...dataConfig
 	},
 	migrations: {
 		directory: path.resolve(__dirname, 'app/store/migrations')
